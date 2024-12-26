5695c257a80d78e27242f006d08d85c39b5aaa90
Yan-Ying Liao <n9102125@gmail.com>
2024-12-26 19:17:39 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Workbook } from "@fortune-sheet/react";
+import "@fortune-sheet/react/dist/index.css";
+import DrawerMenu from "./DrawerMenu"; // Import DrawerMenu
+import { IconButton, Link } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function App() {
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const [data, setData] = useState([
+    {
+      name: "工作表1",
+      celldata: [
+        { r: 0, c: 0, v: { v: "日期", m: "日期" } },
+        { r: 0, c: 1, v: { v: "姓名", m: "姓名" } },
+        { r: 0, c: 2, v: { v: "數字", m: "數字" } },
+        { r: 0, c: 3, v: { v: "公司", m: "公司" } },
+        { r: 0, c: 5, v: { v: "用途", m: "用途" } },
+        { r: 0, c: 6, v: { v: "經理人", m: "經理人" } },
+        { r: 1, c: 0, v: { v: "2022/11/1", m: "2022/11/1" } },
+        { r: 1, c: 1, v: { v: "小沈", m: "小沈" } },
+        { r: 1, c: 2, v: { v: "1500", m: "1500" } },
+      ],
+    },
+  ]);
+
+  const toggleDrawer = (open: boolean) => {
+    setDrawerOpen(open);
+  };
+
+  const handleWorkbookChange = (updatedData: any) => {
+    setData(updatedData); // Update the entire data when Workbook changes
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <div style={{ display: "flex", height: "100vh", flexDirection: "column" }}>
+      {/* Top Bar */}
+      <div
+        style={{
+          height: "50px",
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#f5f5f5",
+          padding: "0 10px",
+        }}
+      >
+        <IconButton onClick={() => toggleDrawer(true)}>
+          <MenuIcon />
+        </IconButton>
+        <span style={{ marginLeft: "10px", fontSize: "16px" }}>Excel Pay</span>
+
+        <Link
+          href="https://www.ptt.cc/bbs/Gossiping/M.1735226140.A.A94.html"
           target="_blank"
           rel="noopener noreferrer"
+          color="primary"
+          underline="hover"
+          style={{
+            marginLeft: "10px",
+            fontSize: "16px",
+            fontWeight: "bold",
+            textDecoration: "none",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          教學
+        </Link>
+
+        <IconButton
+          href="https://github.com/noworneverev/excel-pay"
+          target="_blank"
+          rel="noopener noreferrer"
+          color="inherit"
+          style={{ marginLeft: "auto" }}
+        >
+          <GitHubIcon />
+        </IconButton>
+      </div>
+
+      {/* Drawer Menu */}
+      <DrawerMenu
+        isOpen={isDrawerOpen}
+        toggleDrawer={toggleDrawer}
+        data={data} // Pass the updated number
+      />
+
+      {/* Workbook */}
+      <div style={{ flex: 1, overflow: "auto" }}>
+        <Workbook data={data} onChange={handleWorkbookChange} />
+      </div>
     </div>
   );
 }
---
+++ src/DrawerMenu.tsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+import {
+  Drawer,
+  List,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+  ListItemButton,
+  Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+} from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import FolderOpenIcon from "@mui/icons-material/FolderOpen";
+import ShareIcon from "@mui/icons-material/Share";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import WindowIcon from "@mui/icons-material/Window";
+
+type DrawerMenuProps = {
+  isOpen: boolean;
+  toggleDrawer: (open: boolean) => void;
+  data: any;
+};
+
+const DrawerMenu: React.FC<DrawerMenuProps> = ({
+  isOpen,
+  toggleDrawer,
+  data,
+}) => {
+  const [exportClicked, setExportClicked] = useState(false);
+  const [accountClicked, setAccountClicked] = useState(false);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleExportClick = () => {
+    setExportClicked(true);
+  };
+
+  const handleAccountClick = () => {
+    if (exportClicked) {
+      setDialogOpen(true);
+    }
+    setAccountClicked(true);
+  };
+
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+    setExportClicked(false);
+    setAccountClicked(false);
+  };
+
+  return (
+    <>
+      <Drawer anchor="left" open={isOpen} onClose={() => toggleDrawer(false)}>
+        <Box
+          sx={{
+            width: 250,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+            height: "100%",
+          }}
+        >
+          {/* Main Menu */}
+          <List>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="常用" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemIcon>
+                <InsertDriveFileIcon />
+              </ListItemIcon>
+              <ListItemText primary="新增" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemIcon>
+                <FolderOpenIcon />
+              </ListItemIcon>
+              <ListItemText primary="開啟" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemIcon>
+                <ShareIcon />
+              </ListItemIcon>
+              <ListItemText primary="共用" />
+            </ListItemButton>
+            <Divider />
+            <ListItemButton disabled>
+              <ListItemIcon>
+                <WindowIcon />
+              </ListItemIcon>
+              <ListItemText primary="取得增益集" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemText sx={{ pl: "55px" }} primary="資訊" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemText sx={{ pl: "55px" }} primary="儲存檔案" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemText sx={{ pl: "55px" }} primary="另存新檔" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemText sx={{ pl: "55px" }} primary="列印" />
+            </ListItemButton>
+            <ListItemButton onClick={handleExportClick}>
+              <ListItemText sx={{ pl: "55px" }} primary="匯出" />
+            </ListItemButton>
+            <ListItemButton disabled>
+              <ListItemText sx={{ pl: "55px" }} primary="關閉" />
+            </ListItemButton>
+          </List>
+
+          {/* Bottom Items */}
+          <Box>
+            <Divider />
+            <List>
+              <ListItemButton onClick={handleAccountClick}>
+                <ListItemText sx={{ pl: "55px" }} primary="帳戶" />
+              </ListItemButton>
+              <ListItemButton disabled>
+                <ListItemText sx={{ pl: "55px" }} primary="選項" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+
+      {/* Dialog */}
+      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>提示</DialogTitle>
+        <DialogContent>
+          {data?.[0]?.data?.[1]?.[2]?.v
+            ? `${data[0].data[1][2].v}萬已匯到指定帳戶！`
+            : "資料不可用"}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose} color="primary">
+            關閉
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default DrawerMenu;
